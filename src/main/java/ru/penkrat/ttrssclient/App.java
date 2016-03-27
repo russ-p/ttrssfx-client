13a51f023c9ed9488817fc7d3ccbf578f4f4c7a1
ruslan <ruslan@ruslan-laptop.area51>
2016-03-27 08:44:18 +0300

    title binding

--- src/main/java/ru/penkrat/ttrssclient/App.java
+++ src/main/java/ru/penkrat/ttrssclient/App.java
@@ -2,6 +2,10 @@ package ru.penkrat.ttrssclient;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
+import org.fxmisc.easybind.EasyBind;
+
 import com.google.inject.Module;
 
 import de.saxsys.mvvmfx.FluentViewLoader;
@@ -9,9 +13,14 @@ import de.saxsys.mvvmfx.guice.MvvmfxGuiceApplication;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import ru.penkrat.ttrssclient.domain.Feed;
+import ru.penkrat.ttrssclient.ui.feedstree.FeedScope;
 import ru.penkrat.ttrssclient.ui.view.MainView;
 
 public class App extends MvvmfxGuiceApplication {
+	
+	@Inject
+	private FeedScope feedScope;
 
 	public static void main(String... args) {
 		launch(args);
@@ -19,13 +28,16 @@ public class App extends MvvmfxGuiceApplication {
 
 	@Override
 	public void startMvvmfx(final Stage stage) throws Exception {
-		stage.setTitle("Tiny-Tiny RSS FX");
+		stage.titleProperty().bind(EasyBind.monadic(feedScope.selectedFeedProperty())
+				.map(Feed::getTitle)
+				.map(feedTitle -> "Tiny-Tiny RSS FX - " + feedTitle)
+				.orElse("Tiny-Tiny RSS FX"));
 
 		final Parent parent = FluentViewLoader.fxmlView(MainView.class).load().getView();
 
 		Scene scene = new Scene(parent);
 		scene.getStylesheets().add("/styles/styles.css");
-
+		
 		stage.setScene(scene);
 		stage.show();
 	}
